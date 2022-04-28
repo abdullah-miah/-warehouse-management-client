e5fca284a89685c620526dabe7f006af8e5c690d
abdullah <mdabdullaha708@gmail.com>
2022-04-28 15:31:51 +0600

    Final commit

--- src/componets/CheckOut/CheckOUt.js
+++ src/componets/CheckOut/CheckOUt.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CheckOut/CheckOUt'
 
 const CheckOUt = () => {
     const navigate =useNavigate();
+    const [agree, setAgree]=useState(false);
+    
+    const confirmOrder=()=>{
+        navigate('/orderConfirm')
+    }
+    
     return (
         <div className='w-75 mx-auto mt-4'>
             <h2 className='text-center text-primary '>Booking Confirmation</h2>
@@ -11,7 +17,7 @@ const CheckOUt = () => {
             <div>
                 <div>
                     <label>Full Name</label>
-                     <input type='text' name='name' placeholder='Full name' required></input>
+                     <input  type='text' name='name' placeholder='Full name' required></input>
                     <label>Email Adress</label>
                      <input type='email' name='email' placeholder='Email Adress' required></input>
                     <label>Phone Number</label>
@@ -19,11 +25,13 @@ const CheckOUt = () => {
                      <label>Payment Method</label>
                      <input type='text' name='payment' placeholder='Bkash/Rokect/Cash/Visa' required></input>
                     <label> Write Full Adress</label>
-                     <textarea cols='50' rows='5' required>
-                         Write your Full Adress...........
+                     <textarea onClick={()=>setAgree(!agree)} cols='50' rows='5' required>
+                         Write your Full Address ...........
                      </textarea>
                      <br/>
-                     <button onClick={()=>navigate('/orderConfirm')} className='btn btn-primary mt-3 '>Confirm Booking</button>
+                     <button
+                     disabled={!agree}
+                     onClick={confirmOrder} className='btn btn-primary mt-3 '>Confirm Booking</button>
                 </div>
             </div>
              
