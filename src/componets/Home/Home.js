f7b4bd347c4d083c1432851fbc53b7f82c6d080a
abdullah <mdabdullaha708@gmail.com>
2022-05-08 10:56:57 +0600

    manage page completed

--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -1,28 +1,21 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Service from '../Service/Service';
-import "./Home.css"
-import { servicesContext } from '../../App';
-import AddProducts from '../AddProducts/AddProducts';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const [services, setServices]=useContext(servicesContext);
+    const [products, setProducts] =useState([]);
+    const navigate = useNavigate();
     useEffect(()=>{
-        fetch('service.json')
-        .then(res=>res.json())
-        .then(data =>setServices(data))
-    },[])
+        const fatchData = async ()=>{
+            const {data}= await axios.get('http://localhost:5000/management');
+            setProducts(data.data);
+        }
+        fatchData();
+    },[]);
     return (
         <div>
-            <h1 className='text-center my-4 text-primary'> Provider Services</h1>
-            <div className='service container'>
-                {
-                    services.map(service=> <Service
-                    key={service.id}
-                    service={service}
-                    ></Service>)
-                }
-
-            </div>
+            <h1> Total Product: {products.length}</h1>
+            
         </div>
     );
 };
