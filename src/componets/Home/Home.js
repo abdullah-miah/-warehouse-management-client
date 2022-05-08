d747a5484b9a03c1a06b61c56e9a46d6a2804e21
abdullah <mdabdullaha708@gmail.com>
2022-05-08 11:42:32 +0600

    Home page card

--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Card, CardGroup, Col, Container, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import CartProduct from '../cartProduct/CartProduct';
 
 const Home = () => {
     const [products, setProducts] =useState([]);
@@ -13,9 +15,27 @@ const Home = () => {
         fatchData();
     },[]);
     return (
-        <div>
-            <h1> Total Product: {products.length}</h1>
+        <div className='container w-75'>
+            <h1>Available Stock Product</h1>
+           <div>
+           <Container>
+  <Row>
+    
+    {
+                products.map(product=><CartProduct
+                key={product._id}
+                product ={product}
+                ></CartProduct>)
+            }
+           
+  
+    
+  </Row>
+</Container>
+          
             
+            
+        </div>
         </div>
     );
 };
--- src/componets/cartProduct/CartProduct.js
+++ src/componets/cartProduct/CartProduct.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { Card, CardGroup, ListGroup, ListGroupItem } from 'react-bootstrap';
 
-const CartProduct = () => {
+const CartProduct = ({product}) => {
+    const {name, description, image, price, Quantatity, Supplier, deliver}= product;
     return (
-        <div className='container'>
-            <CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  
-</CardGroup>
+        <div>
+<Col>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Cras justo odio</ListGroupItem>
+    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+</Col>
         </div>
     );
 };
